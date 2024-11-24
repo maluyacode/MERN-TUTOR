3304af968b1f338acd41ea8253176af15824b17a
maluyacode <adlawandavemerc98@gmail.com>
2024-11-24 10:37:26 +0800

    push configs

---
+++ backend/config/dbConnect.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const dbConnect = () => {
+
+    mongoose.connect(process.env.DB_URI).then((con) => {
+
+        console.log(`Connected: ${con.connection.host}`)
+
+    })
+
+}
+
+module.exports = dbConnect;
